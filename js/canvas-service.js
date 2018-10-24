e95849400345704d2598fb8ed9476a74a0ffe338
RotemKaslasi <ksl.rotem@gmail.com>
2018-10-24 12:41:04 +0300

    media css

--- js/canvas-service.js
+++ js/canvas-service.js
@@ -6,9 +6,17 @@ var gCtx;
 
 function createCanvas() {
     gCanvas = document.querySelector('#canvas');
-    gCanvas.width = window.innerWidth / 2;
-    gCanvas.height = window.innerHeight / 2;
+    gCanvas.width = window.innerWidth * 1 / 2;
+    gCanvas.height = window.innerHeight * 1 / 2;
     gCtx = gCanvas.getContext('2d');
+
+    if (window.innerWidth <= 550) {
+        gCanvas = document.querySelector('#canvas');
+        gCanvas.width = window.innerWidth * 5/6;
+        gCanvas.height = window.innerHeight * 5/6;
+        gCtx = gCanvas.getContext('2d');
+
+    }
 }
 
 function drawImage() {
@@ -17,12 +25,12 @@ function drawImage() {
     // const imageDiamRat = img.width / img.height;
     img.onload = function () {
         // gCtx.drawImage(img, 0,0, gCanvas.width, gCanvas.height)
-        
-            var hRatio = gCanvas.width / img.width    ;
-            var vRatio = gCanvas.height / img.height  ;
-            var ratio  = Math.min ( hRatio, vRatio );
-            gCtx.drawImage(img, 0,0, img.width, img.height, 0,0,img.width*ratio, img.height*ratio);
-            // document.querySelector('.sideContainer').style.display = 'inline-grid';
+
+        var hRatio = gCanvas.width / img.width;
+        var vRatio = gCanvas.height / img.height;
+        var ratio = Math.min(hRatio, vRatio);
+        gCtx.drawImage(img, 0, 0, img.width, img.height, 0, 0, img.width * ratio, img.height * ratio);
+        // document.querySelector('.sideContainer').style.display = 'inline-grid';
     }
     img.src = imgUrl;
 }
